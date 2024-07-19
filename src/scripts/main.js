e1909e96edfa83cb09904840a2b22dd455ddcc95
Gabriel Lira <gabriel.tertulino@proton.me>
2024-07-19 11:24:00 -0300

    V6.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,18 @@
 const fetchData = fetch('../../data.json')
   .then(response => response.json())
   .then(data => {
+    updateUI(data, 'weekly');
+
+    document.getElementById('daily-button').addEventListener('click', () => {
+      updateUI(data, 'daily');
+    });
+
+    document.getElementById('weekly-button').addEventListener('click', () => {
+      updateUI(data, 'weekly');
+
+    })
     
   })
   .catch(error => {
-    console.log('Error fetching the JSON:' error)
+    console.log('Error fetching the JSON:', error)
   })
