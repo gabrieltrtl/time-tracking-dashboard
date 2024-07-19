44db4a78d0b84b0a46b8562a900fc9faa67c1dc0
Gabriel Lira <gabriel.tertulino@proton.me>
2024-07-19 14:15:14 -0300

    V7.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+
+//Fetch Requisition
 const fetchData = fetch('../../data.json')
   .then(response => response.json())
   .then(data => {
@@ -9,10 +11,37 @@ const fetchData = fetch('../../data.json')
 
     document.getElementById('weekly-button').addEventListener('click', () => {
       updateUI(data, 'weekly');
+    });
 
-    })
+    document.getElementById('monthly-button').addEventListener('click', () => {
+      updateUI(data, 'monthly');
+    });
     
   })
   .catch(error => {
     console.log('Error fetching the JSON:', error)
   })
+//End Fetch Requisition
+function updateUI(data, timeframe) {
+  const listItems = document.querySelectorAll('.list__item');
+
+  let pastText;
+
+  if (timeframe === 'daily') {
+    pastText = 'Last Day';
+  } else if (timeframe === 'weekly') {
+    pastText = 'Last Week';
+  } else if (timeframe === 'monthly') {
+    pastText = 'Last Month'
+  };
+
+
+  listItems.forEach((listItem, index) => {
+    const itemData = data[index];
+    
+    listItem.querySelector('.item__title').textContent = itemData.title;
+
+    listItem.querySelector('.current-count').textContent = `Current: ${itemData.timeframes[timeframe].current}hrs`;
+    listItem.querySelector('.past-count').textContent = `${pastText} - ${itemData.timeframes[timeframe].previous}hrs`;
+  });
+}
